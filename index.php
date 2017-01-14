873f7574bd786c8c31997c158217bca1150eaba1
fde-monc <fde-monc@student.42.fr>
2017-01-14 18:53:50 +0100

    home ready

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php $inc = './template_parts/'; ?>
+<HTML>
+	<?php include ($inc."head.html") ?>
+	<BODY>
+		<?
+		include($inc."header.html");
+		include($inc."nav.html");
+		?>
+		<div class="main">
+		</div>
+	</BODY>
+</HTML>
